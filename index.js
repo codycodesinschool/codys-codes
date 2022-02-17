e56ae826091b3504bd2ce3d72f8b85ba550e3046
codycodesinschool <97536320+codycodesinschool@users.noreply.github.com>
2022-02-17 11:59:14 -0500

    add files

---
+++ index.js
@@ -0,0 +1,10 @@
+let sites = ["Emoji", "shake", "story","introduction","imageMap","button"];
+let linkdiv = document.querySelector('.links');
+for(let site of sites) {
+  el = document.createElement('a')
+  el.href = "/sites/" + site;
+  el.innerHTML = site
+  linkdiv.appendChild(el);
+  br = document.createElement('br');
+  linkdiv.appendChild(br)
+}
