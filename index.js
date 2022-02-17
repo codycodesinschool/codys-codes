abe7dcbb31e75145764f3c17ecf5c16e8cf6ef3d
codycodesinschool <97536320+codycodesinschool@users.noreply.github.com>
2022-02-17 12:09:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let sites = ["Emoji", "shake", "story","introduction","imageMap","button"];
 let linkdiv = document.querySelector('.links');
 for(let site of sites) {
   el = document.createElement('a')
-  el.href = "/codys-codes/sites/" + site;
+  el.href = "https://codycodesinschool.github.io/codys-codes/sites/" + site
   el.innerHTML = site
   linkdiv.appendChild(el);
   br = document.createElement('br');
