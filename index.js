8d31e8d5a211ce3d9f58adf11cc833f318f2341e
codycodesinschool <97536320+codycodesinschool@users.noreply.github.com>
2022-03-08 21:29:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let sites = ["Emoji", "shake", "story","introduction","imageMap","button","matrix"];
+let sites = ["Emoji", "shake", "story","introduction","imageMap","button","matrix","DreamJob"];
 let linkdiv = document.querySelector('.links');
 for(let site of sites) {
   el = document.createElement('a')
